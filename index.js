e185143798ba9da3795b672df6f4e491a05c44da
MazkaAndrii <andriy.mazka@gmail.com>
2023-04-07 16:19:04 +0300

    add CLI

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 const contactsOperations = require("./contacts");
+
+const { program } = require("commander");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      if (!contact) {
+        throw new Error(`Contaсt with ID ${id} not found`);
+      }
+      console.table(contact);
+      break;
+
+    case "add":
+      const newContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone
+      );
+      console.table(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsOperations.removeContact(id);
+      console.table(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const options = program.opts();
+invokeAction(options);
